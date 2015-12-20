443e7d3b70da8e2cae14d46ae9f1817375463841
Ivo Bek <bekivo@gmail.com>
2015-12-20 15:34:01 +0100

    vacation list works

--- database/run.sh
+++ database/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker rm -f mongo-vacation-planner
-docker run -p 27017:27017 -v /home/ibek/Temp/mongodb/data:/data/db --name mongo-vacation-planner -d mongo --smallfiles
+docker run -p 27017:27017 -v /tmp/mongodb/data:/data/db --name mongo-vacation-planner -d mongo --smallfiles
